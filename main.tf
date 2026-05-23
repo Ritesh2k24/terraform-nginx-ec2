dae7f84c7c3112d52328869615ffb77bf5f0f6c7
RiteshG24AI1050 <g24ai1050@iitj.ac.in>
2026-05-23 09:31:55 +0000

    9th commit

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_subnet" "public_subnet" {
 
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
